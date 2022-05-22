161414cb6c51087dafbc116eb37b6dd7db9cbb45
aungbophyoe <abbp1001@gmail.com>
2022-05-22 19:26:50 +0630

    fix

--- app/src/main/java/com/aungbophyoe/space/words/binding/SettingPageBinding.kt
+++ app/src/main/java/com/aungbophyoe/space/words/binding/SettingPageBinding.kt
@@ -3,9 +3,7 @@ package com.aungbophyoe.space.words.binding
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
-object SettingPageBinding {
-    @BindingAdapter("textKey")
-    fun TextView.setText(key:Int){
-        text = "Value : $key"
-    }
+@BindingAdapter("textData")
+fun TextView.setData(key:Int){
+    text = "Value : $key"
 }
